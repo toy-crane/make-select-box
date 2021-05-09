63cafe6fe7e99b1842f0152750095be18695f46e
toycrane <alwaysfun2183@gmail.com>
2021-05-09 22:39:15 +0900

    custom select box 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const OPTIONS = [
+	{ value: "apple", name: "사과" },
+	{ value: "banana", name: "바나나" },
+	{ value: "orange", name: "오렌지" },
+];
+
+const SelectBoxWrapper = styled.div`
+	display: flex;
+`;
+
+export const Select = styled.select`
+	margin: 0;
+	min-width: 0;
+	display: block;
+	width: 100%;
+	padding: 8px 8px;
+	font-size: inherit;
+	line-height: inherit;
+	border: 1px solid;
+	border-radius: 4px;
+	color: inherit;
+	background-color: transparent;
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	appearance: none;
+	&:focus {
+		border-color: red;
+	}
+`;
+
+const IconSVG = styled.svg`
+	margin-left: -28px;
+	align-self: center;
+	width: 24px;
+	height: 24px;
+`;
+
+const SelectBox = (props) => {
+	const handleChange = (e) => {
+		// event handler
+		console.log(e.target.value);
+	};
+
+	return (
+		<SelectBoxWrapper>
+			<Select onChange={handleChange}>
+				{props.options.map((option) => (
+					<option
+						key={option.value}
+						value={option.value}
+						defaultValue={props.defaultValue === option.value}
+					>
+						{option.name}
+					</option>
+				))}
+			</Select>
+			<IconSVG
+				width="20"
+				height="20"
+				viewBox="0 0 20 20"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M10 14L16 6H4L10 14Z"
+					fill="#1A1A1A"
+				/>
+			</IconSVG>
+		</SelectBoxWrapper>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <SelectBox options={OPTIONS} defaultValue="banana"></SelectBox>;
 }
 
 export default App;
